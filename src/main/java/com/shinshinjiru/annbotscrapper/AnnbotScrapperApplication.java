f8bdedc511993a2c99b3a5c2c24e9acf15a6abdb
manulaiko <manulaiko@gmail.com>
2020-09-19 14:52:58 +0200

    Initial commit

---
+++ src/main/java/com/shinshinjiru/annbotscrapper/AnnbotScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.shinshinjiru.annbotscrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnbotScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnbotScrapperApplication.class, args);
+	}
+
+}
